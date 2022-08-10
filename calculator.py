9949906bfd6ce1fe982e54a19e19ed9e14b45371
sreekanth <ksaisreekanth@gmail.com>
2022-08-10 22:18:35 +0530

    first commit from pycharm

---
+++ calculator.py
@@ -0,0 +1,6 @@
+# simple one
+
+number_1 = float( input('Enter a number: ') )
+number_2 = float( input('Enter a number: ') )
+
+print('the sum of 2 numbers is', number_1 + number_2 )
---
+++ day_1.py
@@ -0,0 +1,21 @@
+## variables
+age = 20
+price = 20.109
+name = 'Sreekanth'
+is_online = False
+
+print(type(price))
+
+# for strings, or similar Data types, + can be used
+# for others, use comma ,
+print('Hello World' + '\nI am', age)
+
+name = input("What is your name: ")
+
+print('hello ' + name)
+
+birth_year = input('Enter your birth year: ')
+
+age = 2022 - int(birth_year)
+
+print('you are ', age , 'years old')
